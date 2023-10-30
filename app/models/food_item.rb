eb0392a3d4d3d757a6379093874a7da58a1fc257
cstudeo <cstudeo@users.noreply.github.com>
2023-10-30 08:40:47 -0400

    rebasiing of active_storage config branch

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,9 +1,13 @@
 class FoodItem < ApplicationRecord
   belongs_to :restaurant
-  has_one_attached :image
+  has_one_attached :picture
 
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "name", "price", "restaurant_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["restaurant", "picture_attachment", "picture_blob"]
   end
 end
