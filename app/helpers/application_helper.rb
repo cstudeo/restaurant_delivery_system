e2da57e3efb39990c2a27ab6776c89ac91cc4831
cstudeo <cstudeo@users.noreply.github.com>
2023-11-02 14:44:10 -0400

    wip on delivery guy dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_name
+    user = current_user
+    "#{user.first_name} #{user.last_name}"
+  end
 end
---
+++ db/migrate/20231101182343_add_verified_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddVerifiedColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :is_verified, :boolean, default: false
+  end
+end
