8984e70d4e5a4c4d0423082fa6f951ffa80f81e6
cstudeo <cstudeo@users.noreply.github.com>
2023-11-06 07:01:09 -0800

    temp commit to switch system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,41 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :create_order_items, only: [:create], if: -> { controller_path == 'users/sessions' }
 
   private
 
   def after_sign_in_path_for(user)
     return carriers_path if user.instance_of? Carrier
     return admin_dashboard_path if user.instance_of? AdminUser
+    return root_path if user.instance_of? Customer
+
+    # stored_location = stored_location_for(user)
+
+    # if stored_location
+    #   stored_location
+    # elsif user.instance_of?(Carrier)
+    #   carriers_path
+    # elsif user.instance_of?(AdminUser)
+    #   admin_dashboard_path
+    # else
+    #   root_path
+    # end
+  end
+
+  def create_order_items
+    order_details = session[:order_details]
+    if order_details
+      order_details.each do |order_detail|
+        food_item_id = order_detail["food_item_id"]
+        quantity = order_detail["quantity"]
+        current_cart.order_items.create(
+          food_item_id: food_item_id,
+          quantity: quantity
+        )
+      end
+      session.delete(:order_details)
+    end
   end
 
   def configure_permitted_parameters
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,52 @@
+class OrdersController < ApplicationController
+    before_action :authenticate_user!, only: [:create]
+    before_action :set_cart, only: [:create]
+    before_action :set_restaurant, only: [:create]
+    before_action :set_customer, only: [:create]
+    before_action :set_carrier, only: [:create]
+
+    def create
+        # store_location_for(:user, request.fullpath)
+        # authenticate_user!
+        # byebug
+
+        @order = Order.new(
+            customer: @customer,
+            restaurant: @restaurant,
+            carrier: @carrier,
+            total_amount: @cart.total_amount
+        )
+        if @order.save
+            @cart.order_items.each do |order_item|
+              order_item.update(order_id: @order.id, cart_id: nil)
+            end
+            redirect_to order_path(@order)
+        else
+            flash[:alert] = "Unable to process the order. Please try again later."
+            redirect_to root_path
+        end
+    end
+
+    def show
+        @order = Order.find(params[:id])
+    end
+
+    private
+
+    def set_cart
+        @cart = current_cart
+    end
+
+    def set_restaurant
+        @restaurant = @cart.restaurant
+    end
+
+    def set_customer
+        @customer = current_user
+    end
+
+    def set_carrier
+        @carrier = eligible_carriers
+    end
+  end
+  
