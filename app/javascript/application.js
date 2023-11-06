f55a9c08574f73164adade430b0c85ba790543fd
cstudeo <cstudeo@users.noreply.github.com>
2023-11-07 02:39:31 +0500

    fixed trubo issue with drawer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener("DOMContentLoaded", initializeDrawer);
+document.addEventListener("turbo:load", initializeDrawer);
+
+
+function initializeDrawer() {
+  const drawerButton = document.querySelector('[data-drawer-show="drawer-example"]');
+  const drawer = document.querySelector('#drawer-example');
+  const closeButton = document.querySelector('[data-drawer-hide="drawer-example"]');
+
+  // This is just an example. Your actual logic to show/hide the drawer might be different.
+  drawerButton.addEventListener('click', () => {
+      drawer.style.transform = 'translateX(0)'; // Show the drawer
+  });
+
+  closeButton.addEventListener('click', () => {
+      drawer.style.transform = 'translateX(-100%)'; // Hide the drawer
+  });
+}
