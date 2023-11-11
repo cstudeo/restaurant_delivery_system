a69ff01daed2481226fc0a8a8d58ae4e3213be78
Shan Mark <markshan.exp@gmail.com>
2023-11-11 06:53:56 +0500

    Fixed root path for logo and updated orders history page

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -5,7 +5,6 @@ class Carrier < User
 
   scope :is_available, -> { where(is_available: true, is_verified: true) }
 
-
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "daily_orders_count", "email", "encrypted_password", "first_name", "id", "is_available", "last_name", "phone_number", "remember_created_at", "reset_password_sent_at", "reset_password_token", "type", "updated_at"]
   end
