da08327777cc7253e11a681c43ebd6b80e22286e
cstudeo <cstudeo@users.noreply.github.com>
2023-11-30 01:06:53 +0500

    fixed environment issues

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module FoodShop
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :async
 
     # Configuration for the application, engines, and railties goes here.
     #
